a3d3c60dab0666278745b625e2f305db3e5f1b1e
Jonathan Cheng <jcheng461@gmail.com>
2022-03-08 23:10:40 +0900

    refactor event handlers to work with an array and for each loop.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
 $(document).ready(function () {
-  $("h1").click(function () {
-    alert("This is a heading.");
-  });
+  // This is what I want to replicate, but do it with arrays & loops.
+  // $("h1").click(function () {
+  //   alert("This is a heading.");
+  // });
 
-  $("p").click(function () {
-    alert("This is a paragraph.");
-  });
+  // $("p").click(function () {
+  //   alert("This is a paragraph.");
+  // });
 
-  $("img").click(function () {
-    alert("This is an image.");
+  // $("img").click(function () {
+  //   alert("This is an image.");
+  // });
+  const tags = ["h1", "p", "img"];
+
+  let object;
+  tags.forEach(function (tag) {
+    $(tag).click(function () {
+      if (tag === "h1") {
+        object = "heading";
+      } else if (tag === "p") {
+        object = "paragraph";
+      } else if (tag === "img") {
+        object = "image";
+      }
+      alert("This is a(n) " + object + ".");
+    });
   });
 });
